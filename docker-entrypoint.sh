5e5f65b48e35dad7e300d03170ed75a42ea7e412
randomcoww <etherblimp@gmail.com>
2019-08-21 21:57:29 -0700

    Split mount and config dir

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
 
-config_file=${CONFIG_FILE:-/etc/transmission/settings.json}
+## Working path for transmission
 config_dir=${CONFIG_DIR:-/var/lib/transmission}
+## Mount external volume here and symlink paths to config_dir
+mount_dir=${MOUNT_DIR:-/etc/transmission}
 
-## relocate config_file so that 
-## bnth config_dir and config_file may be mounted without conflict
-ln -fs $config_dir/settings.json $config_file
+## Preserve this data in mounted volume
+mkdir -p $mount_dir/resume
+mkdir -p $mount_dir/torrents
+ln -sf $mount_dir/resume $config_dir/resume
+ln -sf $mount_dir/torrents $config_dir/torrents
 
-## start
+## Config file must be at $config_dir/settings.json
+## This may be mounted from configMap
+
+## Start
 exec transmission-daemon \
-  --config-dir $config_dir \
   $@ \
+  --config-dir $config_dir \
   --foreground \
   --log-error
