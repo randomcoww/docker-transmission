a74baf4c47f65ea1475b4e72e82b21744ec0e42c
randomcoww <etherblimp@gmail.com>
2019-11-21 20:02:41 -0800

    Add .keep files in resume and torrent path

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,8 +8,10 @@ config_dir=${CONFIG_DIR:-/var/lib/transmission}
 torrents_dir=${TORRENTS_DIR:-/transmission/torrents}
 resume_dir=${RESUME_DIR:-/transmission/resume}
 mkdir -p $torrents_dir
+touch $torrents_dir/.keep
 ln -sf $torrents_dir $config_dir/torrents
 mkdir -p $resume_dir
+touch $resume_dir/.keep
 ln -sf $resume_dir $config_dir/resume
 
 ## Config file must be at $config_dir/settings.json
